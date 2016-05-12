161bf834c3015b5738b679a963da64a5530b37f7
RobertBaghai <robertbaghai@gmail.com>
2016-05-12 12:53:05 -0400

    first commit

---
+++ RestaurantApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RestaurantApp
+//
+//  Created by Robert Baghai on 5/12/16.
+//  Copyright © 2016 Robert Baghai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RestaurantApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RestaurantApp
+//
+//  Created by Robert Baghai on 5/12/16.
+//  Copyright © 2016 Robert Baghai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
